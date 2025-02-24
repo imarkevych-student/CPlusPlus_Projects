1eb7d0955855b08ef2e64109e15ee38d2265f71c
imarkevych-student <marke_im25@student.itstep.org>
2025-02-24 20:58:50 +0200

    Task 2 Done

---
+++ 01_Homework.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+
+using namespace std;
+
+void main()
+{
+
+	/*const float inch = 2.54;
+	float diagonalInInch;
+	cout << "Enter diagonal in inch:";
+	cin >> diagonalInInch;
+	cout << "Length of diagonal in cm = " << inch * diagonalInInch;const float inch = 2.54;*/
+
+	float kormPerDay;
+	
+	cout << "Enter korm per day in gramm:";	
+	cin >> kormPerDay;
+	cout << "Count of korm = " << kormPerDay * 30/1000;
+}
