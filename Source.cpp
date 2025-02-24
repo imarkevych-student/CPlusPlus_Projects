34fcd9483b2426718a5268f1a497f44a5f9496a7
imark_f02ge4r <imark_f02ge4r@IVANO-NOTEBOOK>
2025-02-24 20:34:53 +0200

    Add project files.

---
+++ Source.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+
+using namespace std;
+
+void main()
+{
+	/*cout << "Ukraine's glory has not yet perished, nor her freedom,\n"
+		"Upon us, fellow compatriots, fate shall smile once more.\n"
+		" Our enemies will vanish, like dew in the morning sun, \n"
+		"And we too shall rule, brothers, in a free land of our own.\n\n"
+		"We’ll lay down our souls and bodies to attain our freedom, \n"
+		"And we’ll show that we, brothers, are of the Kozak nation.\n"
+		"Let’s stand, brothers, in bloody battle, from the Sian to the Don, \n"
+		"We will not allow others to rule in our motherland.";*/
+
+	//cout << "Every\n \thunter\n \t\twants\n \t\t\tto\n \t\t\t\tknow\n \t\t\t\t\twhere \n\t\t\t\t\t\tthe\n \t\t\t\t\t\t\tpheasant \n\t\t\t\t\t\t\t\t\tis.";
+
+	cout << "\t\t\tSelling puppies.\n\t\t\tBreed: Rottweiler.\n";	
+	cout << "\t\t\tTel.: 222-22-22\n";
+	cout << "\t\t\t222 | 222 | 222\n";
+	cout << "\t\t\t22  | 22  | 22\n" ;
+	cout << "\t\t\t22  | 22  | 22\n" ;
+
+
+}
